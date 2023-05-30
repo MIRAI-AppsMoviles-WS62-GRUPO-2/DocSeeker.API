e4d3b436e01849596017efbe802dc2586f2848ab
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-05-30 12:46:16 -0500

    feat: add Prescription entity

---
+++ DocSeeker.API/Prescriptions/Domain/Models/Prescription.cs
@@ -0,0 +1,19 @@
+﻿namespace DocSeeker.API.Prescriptions.Domain.Models;
+
+public class Prescription
+{
+    public Prescription(List<Medicine> medicines)
+    {
+        Medicines = medicines;
+    }
+
+    public int Id { get; set; }
+    public string State { get; set; }
+    public string Recommendation { get; set; }
+
+    // Relationships
+    public int DoctorId { get; set; }
+    public Doctor Doctor { get; set;} 
+    
+    public List<Medicine> Medicines { get; set; }
+}
