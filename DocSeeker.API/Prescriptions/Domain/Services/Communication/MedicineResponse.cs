91a9f81277936783e174f25e900cc4c674acf5a2
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-05-30 12:52:43 -0500

    feat: add Doctor and Medicine Response for communication

---
+++ DocSeeker.API/Prescriptions/Domain/Services/Communication/MedicineResponse.cs
@@ -0,0 +1,15 @@
+﻿using DocSeeker.API.Prescriptions.Domain.Models;
+using DocSeeker.API.Shared.Domain.Services.Communication;
+
+namespace DocSeeker.API.Prescriptions.Domain.Services.Communication;
+
+public class MedicineResponse: BaseResponse<Medicine>
+{
+    public MedicineResponse(string message) : base(message)
+    {
+    }
+
+    public MedicineResponse(Medicine resource) : base(resource)
+    {
+    }
+}
