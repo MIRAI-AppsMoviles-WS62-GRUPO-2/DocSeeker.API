86a395140a50c6f06b21c75fca08c01149fd021c
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-05-30 17:51:40 -0500

    feat: create doctor, medicine and prescription repository contracts

---
+++ DocSeeker.API/Prescriptions/Domain/Repositories/IMedicineRepository.cs
@@ -0,0 +1,12 @@
+﻿using DocSeeker.API.Prescriptions.Domain.Models;
+
+namespace DocSeeker.API.Prescriptions.Domain.Repositories;
+
+public interface IMedicineRepository
+{
+    Task<IEnumerable<Medicine>> ListAsync();
+    Task<IEnumerable<Medicine>> FindByPrescriptionIdAsync(int prescriptionId);
+    Task AddAsync(Medicine medicine);
+    void Update(Medicine medicine);
+    void Remove(Medicine medicine);
+}
