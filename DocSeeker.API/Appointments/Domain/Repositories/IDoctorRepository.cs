838f2d733b028d73808d2ac03d22fda18d000b1c
u201817461 <u201817461@upc.edu.pe>
2023-05-31 17:38:18 -0500

    feat: appointments repositories

---
+++ DocSeeker.API/Appointments/Domain/Repositories/IDoctorRepository.cs
@@ -0,0 +1,12 @@
+﻿using DocSeeker.API.Appointments.Domain.Models;
+
+namespace DocSeeker.API.Appointments.Domain.Repositories;
+
+public interface IDoctorRepository
+{
+    Task<IEnumerable<Doctor>> ListAsync();
+    Task AddAsync(Doctor doctor);
+    Task<Doctor> FindByIdAsync(int id);
+    void Update(Doctor doctor);
+    void Remove(Doctor doctor);
+}
