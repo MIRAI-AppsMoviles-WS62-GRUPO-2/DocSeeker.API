86d821d6bf92e0cc3e7f66b9272cf1e4356fdac2
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-05-30 23:09:18 -0500

    chore: added object mapping rules for prescription bounded context

---
+++ DocSeeker.API/Prescriptions/Mapping/ModelToResourceProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using DocSeeker.API.Prescriptions.Domain.Models;
+using DocSeeker.API.Prescriptions.Resources;
+
+namespace DocSeeker.API.Prescriptions.Mapping;
+
+public class ModelToResourceProfile: Profile
+{
+    protected ModelToResourceProfile()
+    {
+        CreateMap<Prescription, PrescriptionResource>();
+        CreateMap<Medicine, MedicineResource>();
+        CreateMap<Doctor, DoctorResource>();
+    }
+}
---
+++ DocSeeker.API/Prescriptions/Mapping/ResourceToModelProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using DocSeeker.API.Prescriptions.Domain.Models;
+using DocSeeker.API.Prescriptions.Resources;
+
+namespace DocSeeker.API.Prescriptions.Mapping;
+
+public class ResourceToModelProfile: Profile
+{
+    protected ResourceToModelProfile()
+    {
+        CreateMap<SavePrescriptionResource, Prescription>();
+        CreateMap<SaveMedicineResource, Medicine>();
+        CreateMap<SaveDoctorResource, Doctor>();
+    }
+}
