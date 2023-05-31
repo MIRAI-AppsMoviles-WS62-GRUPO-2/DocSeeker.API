dd9465ff3bd5c954164a2507de03e3a9ec23f268
u201817461 <u201817461@upc.edu.pe>
2023-05-31 17:45:59 -0500

    feat: resources to incoming and outgoing transfer objects

---
+++ DocSeeker.API/Appointments/Resources/SaveDoctorResource.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DocSeeker.API.Appointments.Resources;
+
+public class SaveDoctorResource
+{
+    [Required]
+    public string Name { get; set; }
+    
+    [Required]
+    public string Specialization { get; set; }
+    
+    [Required]
+    public string PhoneNumber { get; set; }
+    
+    [Required]
+    public string Email { get; set; }
+}
