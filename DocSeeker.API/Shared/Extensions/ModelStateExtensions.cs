b6fda5cb2a1c8cc817bbafdd51f39820a6dda2dc
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-05-31 01:56:08 -0500

    chore: added model state extension for getting one error message collection from state validation.

---
+++ DocSeeker.API/Shared/Extensions/ModelStateExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace DocSeeker.API.Shared.Extensions;
+
+// The external elements that arrive (Example: SavePrescriptionResource) will be
+// validated (required and maxlengh) and for each property that fails, it returns
+// a collection of errors. But we want a single collection of errors to be
+// returned for each entity. So we are going to extend a class called ModelState.
+public static class ModelStateExtensions
+{
+    public static List<string> GetErrorMessages(this ModelStateDictionary dictionary)
+    {
+        return dictionary
+            .SelectMany(m => m.Value.Errors)
+            .Select(m => m.ErrorMessage)
+            .ToList();
+    }
+}
