d1a4b10c1852d9135a3739bef230f9cd570b3950
Erick <u201910151@upc.edu.pe>
2023-05-31 19:08:37 -0500

    feat: change the entity Patient

--- DocSeeker.API/Profile/Domain/Models/Patient.cs
+++ DocSeeker.API/Profile/Domain/Models/Patient.cs
@@ -2,5 +2,16 @@
 
 public class Patient
 {
-    
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string LastName { get; set; }
+
+    public double Height { get; set; }
+
+    public double Weight { get; set; }
+
+    public double BodyMass { get; set; }
+
 }
