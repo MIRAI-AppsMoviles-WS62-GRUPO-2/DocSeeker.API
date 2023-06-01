7a780e8bfd30c943d2134df9e4ed3071b312e0cc
Erick <u201910151@upc.edu.pe>
2023-05-31 19:18:24 -0500

    feat: change MedicalRecord entity

--- DocSeeker.API/Profile/Domain/Models/MedicalRecord.cs
+++ DocSeeker.API/Profile/Domain/Models/MedicalRecord.cs
@@ -2,5 +2,17 @@
 
 public class MedicalRecord
 {
+    public MedicalRecord(List<Allergie> allergies)
+    {
+        Allergies = allergies;
+    }
     
+    public MedicalRecord(List<Pathological> pathologicals)
+    {
+        Pathologicals = pathologicals;
+    }
+    
+    public int Id { get; set; }
+    public List<Allergie> Allergies { get; set; }
+    public List<Pathological> Pathologicals { get; set; }
 }
