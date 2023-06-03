605b5a455816eefcaadea140ac5dbef31d916307
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-06-03 11:58:22 -0500

    feat: added prescriptions context facade.

---
+++ DocSeeker.API/Prescriptions/Services/PrescriptionsContextFacade.cs
@@ -0,0 +1,19 @@
+﻿using DocSeeker.API.Prescriptions.Interfaces.Internal;
+
+namespace DocSeeker.API.Prescriptions.Services;
+
+public class PrescriptionsContextFacade: IPrescriptionsContextFacade
+{
+    private readonly PrescriptionService _prescriptionService;
+    private readonly MedicineService _medicineService;
+    
+    public int TotalPrescriptions()
+    {
+        return _prescriptionService.ListAsync().Result.Count();
+    }
+
+    public int TotalMedicines()
+    {
+        return _medicineService.ListAsync().Result.Count();
+    }
+}
