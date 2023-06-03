2453b8e60cc6829e6c485a4e0f689956a9aae7bf
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-06-03 01:04:26 -0500

    chore: added application configuration.

--- DocSeeker.API/Program.cs
+++ DocSeeker.API/Program.cs
@@ -1,3 +1,12 @@
+using DocSeeker.API.Prescriptions.Domain.Repositories;
+using DocSeeker.API.Prescriptions.Domain.Services;
+using DocSeeker.API.Prescriptions.Persistence.Repositories;
+using DocSeeker.API.Prescriptions.Services;
+using DocSeeker.API.Shared.Domain.Repositories;
+using DocSeeker.API.Shared.Persistence.Contexts;
+using DocSeeker.API.Shared.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +16,50 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+// Add Database Connection
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseMySQL(connectionString)
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        .EnableSensitiveDataLogging()
+        .EnableDetailedErrors());
+
+
+// Lowercase URLs configuration
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+
+// Dependency Injection Configuration
+// Shared Bounded Contexts Injection Configuration
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+
+// Learning Bounded Context Injection Configuration
+builder.Services.AddScoped<IPrescriptionRepository, PrescriptionRepository>();
+builder.Services.AddScoped<IPrescriptionService, PrescriptionService>();
+builder.Services.AddScoped<IMedicineRepository, MedicineRepository>();
+builder.Services.AddScoped<IMedicineService, MedicineService>();
+
+
+// AutoMapper Configuration
+builder.Services.AddAutoMapper(
+    typeof(DocSeeker.API.Prescriptions.Mapping.ModelToResourceProfile),
+    typeof(DocSeeker.API.Prescriptions.Mapping.ResourceToModelProfile));
+
+
+// Application build
 var app = builder.Build();
+
+
+// Validation for ensuring Database Objects are created
+using (var scope = app.Services.CreateScope())
+using (var context = scope.ServiceProvider.GetService<AppDbContext>())
+{
+    context.Database.EnsureCreated();
+}
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
