966db8ec0c0723a6cd8050daefb0135f98b2802d
Erick Armando Cueva Elera <89040344+ECueva1809@users.noreply.github.com>
2023-06-07 21:29:00 -0500

    feat(MedicalRecord): Add bounded context

---
+++ DocSeeker.API/MedicalRecord/Mapping/ModelToResourceProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Docseeker.API.MedicalRecord.Domain.Models;
+using Docseeker.API.MedicalRecord.Resources;
+
+namespace DocSeeker.API.MedicalRecord.Mapping;
+
+public class ModelToResourceProfile : Profile
+{
+    public ModelToResourceProfile()
+    {
+        CreateMap<Record, RecordResource>();
+    }
+}
+
---
+++ DocSeeker.API/MedicalRecord/Resources/RecordResource.cs
@@ -0,0 +1,14 @@
+﻿namespace Docseeker.API.MedicalRecord.Resources;
+
+public class RecordResource
+{
+    public int Id { get; set; }
+    
+    public float Height { get; set; }
+    
+    public float Weight { get; set; }
+    
+    public float BodyMass { get; set; }
+    
+    public int PatientId { get; set; }
+}
