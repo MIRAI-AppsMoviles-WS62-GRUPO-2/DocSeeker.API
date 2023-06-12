6cab2ad08b3a0b396b14711fbee5e2a1b4947906
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-06-12 18:42:11 -0500

    feat: added register and update request data transfer objects.

---
+++ DocSeeker.API/Prescriptions/Interfaces/Rest/Controllers/UpdateRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace DocSeeker.API.Prescriptions.Interfaces.Rest.Controllers;
+
+public class UpdateRequest
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
---
+++ DocSeeker.API/Security/Domain/Services/Communication/RegisterRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DocSeeker.API.Security.Domain.Services.Communication;
+
+// If we want to register a new user, all attributes are required.
+public class RegisterRequest
+{
+    [Required] public string FirstName { get; set; }
+    [Required] public string LastName { get; set; }
+    [Required] public string Username { get; set; }
+    [Required] public string Password { get; set; }
+}
