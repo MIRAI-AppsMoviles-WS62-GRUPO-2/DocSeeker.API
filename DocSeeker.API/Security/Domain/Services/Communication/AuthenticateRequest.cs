cdd1ca80023f02dc037ca5167b581f61727c0227
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-06-12 18:17:29 -0500

    feat: added authenticate request and response Data Transfer Object.

---
+++ DocSeeker.API/Security/Domain/Services/Communication/AuthenticateRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DocSeeker.API.Security.Domain.Services.Communication;
+
+public class AuthenticateRequest
+{
+    [Required] public string Username { get; set; }
+    [Required] public string Password { get; set; }
+}
