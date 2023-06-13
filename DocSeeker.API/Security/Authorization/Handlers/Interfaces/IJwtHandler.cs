bfbdb3b9da71a7cacf2a5fb6f7e5a3e7a25c61d5
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-06-13 10:19:51 -0500

    feat: added token generation capability to jwt handler.

---
+++ DocSeeker.API/Security/Authorization/Handlers/Interfaces/IJwtHandler.cs
@@ -0,0 +1,12 @@
+﻿using DocSeeker.API.Security.Domain.Models;
+
+namespace DocSeeker.API.Security.Authorization.Handlers.Interfaces;
+
+public interface IJwtHandler
+{
+    public string GenerateToken(User user);
+    
+    // If the token is valid, the method can return the ID associated
+    // with the token as an integer. If the token is not valid the method returns null.
+    public int? ValidateToken(string token);
+}
