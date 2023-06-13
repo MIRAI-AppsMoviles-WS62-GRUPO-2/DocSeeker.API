038ffd30e9a3c4341ee98c8d1bb60e39578e0725
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-06-13 13:42:48 -0500

    feat: added user outbound service for persistence.

---
+++ DocSeeker.API/Security/Persistence/UserRepository.cs
@@ -0,0 +1,54 @@
+﻿using DocSeeker.API.Security.Domain.Models;
+using DocSeeker.API.Security.Domain.Repositories;
+using DocSeeker.API.Shared.Persistence.Contexts;
+using DocSeeker.API.Shared.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace DocSeeker.API.Security.Persistence;
+
+public class UserRepository: BaseRepository, IUserRepository
+{
+    public UserRepository(AppDbContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<User>> ListAsync()
+    {
+        return await _context.Users.ToListAsync();
+    }
+
+    public async Task AddAsync(User user)
+    {
+        await _context.Users.AddAsync(user);
+    }
+
+    public async Task<User> FindByIdAsync(int id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
+
+    public async Task<User> FindByUsernameAsync(string username)
+    {
+        return await _context.Users.SingleOrDefaultAsync(x => x.Username == username);
+    }
+
+    public bool ExistsByUsername(string username)
+    {
+        return _context.Users.Any(x => x.Username == username);
+    }
+
+    public User FindById(int id)
+    {
+        return _context.Users.Find(id);
+    }
+
+    public void Update(User user)
+    {
+        _context.Users.Update(user);
+    }
+
+    public void Remove(User user)
+    {
+        _context.Users.Remove(user);
+    }
+}
