df613813e106bdce40148f3c4ac0ea369a0906c7
Lucas Benedetti Rivas <102088086+u201817461@users.noreply.github.com>
2023-06-23 19:00:24 -0500

    feat: merge into master

---
+++ DocSeeker.API/Shared/Domain/Services/Communication/BaseResponse.cs
@@ -0,0 +1,26 @@
+﻿namespace DocSeeker.API.Shared.Domain.Services.Communication;
+
+public abstract class BaseResponse<T>
+{
+    // Unhappy Response
+    protected BaseResponse(string message)
+    {
+        Success = false;
+        Message = message;
+        Resource = default;
+    }
+
+    // Happy Response
+    protected BaseResponse(T resource)
+    {
+        Success = true;
+        Message = string.Empty;
+        Resource = resource;
+    }
+
+    public bool Success { get; protected set; }
+
+    public string Message { get; protected set; }
+
+    public T Resource { get; protected set; }
+}
---
+++ DocSeeker.API/Shared/Extensions/StringExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace DocSeeker.API.Shared.Extensions;
+
+// In this file we create the configuration to transform class nomenclature
+// into database nomenclature. 
+public static class StringExtensions
+{
+    public static string ToSnakeCase(this string text)
+    {
+        static IEnumerable<char> Convert(CharEnumerator e)
+        {
+            if (!e.MoveNext()) yield break;
+
+            yield return char.ToLower(e.Current);
+
+            while (e.MoveNext())
+                if (char.IsUpper(e.Current))
+                {
+                    yield return '_';
+                    yield return char.ToLower(e.Current);
+                }
+                else
+                {
+                    yield return e.Current;
+                }
+        }
+
+        return new string(Convert(text.GetEnumerator()).ToArray());
+    }
+}
